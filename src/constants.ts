bf31ecbef2186a8968f934afdd1956ed23a34e71
Miika Laurila <laurila.miika@gmail.com>
2025-06-08 14:59:40 +0300

    Forgot to change cors prefix

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const defaultSidebarWidth = 200;
 export const defaultTopbarHeight = 40;
 
-// export const corsProxy = "https://cors.lauri.lat/";
-export const corsProxy = "http://localhost:8080/";
+export const corsProxy = "https://cors.lauri.lat/";
+// export const corsProxy = "http://localhost:8080/";
